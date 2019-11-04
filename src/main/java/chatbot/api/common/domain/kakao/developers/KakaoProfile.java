cbcd8e8c1f8385f7d8c06286f21e130b4e1bad54
lee <ejsvk3284@gmail.com>
2019-11-05 01:23:39 +0900

    fix bug about unRecognizedException

--- src/main/java/chatbot/api/common/domain/kakao/developers/KakaoProfile.java
+++ src/main/java/chatbot/api/common/domain/kakao/developers/KakaoProfile.java
@@ -1,5 +1,6 @@
 package chatbot.api.common.domain.kakao.developers;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -8,4 +9,10 @@ import lombok.ToString;
 public class KakaoProfile {
 
     private String nickname;
+
+    @JsonProperty("thumbnail_image_url")
+    private String thumbnailImageUrl;
+
+    @JsonProperty("profile_image_url")
+    private String profileImageUrl;
 }
